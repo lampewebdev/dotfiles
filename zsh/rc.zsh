5f36fcf5f94085c991808bb8b5832744f632b8d4
Michael Lazarski <lampe@Michaels-MacBook-Pro.local>
2023-03-14 21:55:24 +0100

    update

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -32,3 +32,9 @@ export PATH="$PNPM_HOME:$PATH"
 autoload -Uz compinit
 compinit
 source <(ng completion script)
+
+PATH="/Users/lampe/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/Users/lampe/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/Users/lampe/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/Users/lampe/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/Users/lampe/perl5"; export PERL_MM_OPT;
