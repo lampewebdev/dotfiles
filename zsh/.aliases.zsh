6ba4568f0ba2d1da13f435c121c85a574e6dc392
Michael Lazarski <michael.lazarski@scania.com>
2023-05-26 20:43:40 +0200

    update

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -3,6 +3,9 @@ alias ls='exa -l'
 alias sl='exa -l'
 alias c='clear'
 alias v='nvim'
+alias ze='zellij'
+alias zea='zellij a'
+alias zel='zellij ls'
 alias stowh="stow --target $HOME"
 function take {
   mkdir -p $1
