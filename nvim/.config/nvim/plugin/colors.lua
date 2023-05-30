dd753db3008b501c118115d88463c547206b995c
Michael Lazarski <michael.lazarski@scania.com>
2023-05-30 20:07:32 +0200

    update

--- nvim/.config/nvim/plugin/colors.lua
+++ nvim/.config/nvim/plugin/colors.lua
@@ -1,5 +1,6 @@
 -- vim.cmd('colorscheme rose-pine')
 -- vim.cmd('colorscheme dracula')
 -- vim.cmd [[colorscheme tokyonight-night]]
-vim.cmd [[colorscheme catppuccin-mocha]]
+-- vim.cmd [[colorscheme catppuccin-mocha]]
+vim.cmd [[colorscheme kanagawa-wave]]
 require('nvim-highlight-colors').setup {}
