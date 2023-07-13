214e511667aacff299d33eda2f72755c1c34bd23
Michael Lazarski <michael.lazarski@scania.com>
2023-07-13 20:35:53 +0200

    update

--- nvim/.config/nvim/lua/lampe/lazy.lua
+++ nvim/.config/nvim/lua/lampe/lazy.lua
@@ -95,8 +95,42 @@ local plugins = {
         end,
     },
     {
-        'phaazon/hop.nvim',
-        branch = 'v2',
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            {
+                "S",
+                mode = { "n", "o", "x" },
+                function() require("flash").treesitter() end,
+                desc =
+                "Flash Treesitter"
+            },
+            {
+                "r",
+                mode = "o",
+                function() require("flash").remote() end,
+                desc =
+                "Remote Flash"
+            },
+            {
+                "R",
+                mode = { "o", "x" },
+                function() require("flash").treesitter_search() end,
+                desc =
+                "Treesitter Search"
+            },
+            {
+                "<c-s>",
+                mode = { "c" },
+                function() require("flash").toggle() end,
+                desc =
+                "Toggle Flash Search"
+            },
+        },
     }
 }
 local opts = {}
